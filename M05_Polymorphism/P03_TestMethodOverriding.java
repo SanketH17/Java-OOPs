0fb6763e65ae94b4750eb3c39464459733d17922
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-07 15:59:19 +0530

    method overriding

---
+++ M05_Polymorphism/P03_TestMethodOverriding.java
@@ -0,0 +1,31 @@
+package OOPs.M05_Polymorphism;
+
+/*
+Let's understand the problem that we may face in the program if we don't use method overriding.
+
+*/
+
+class Vehicle {
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+class Bike extends Vehicle {
+
+}
+
+
+public class P03_TestMethodOverriding {
+    public static void main(String[] args) {
+        // creating an instance of a child class
+        Bike obj = new Bike();
+        obj.run();
+    }
+}
+
+
+/*
+Problem is that I have to provide a specific implementation of run() method in subclass that is why we use method overriding.
+
+*/
---
+++ M05_Polymorphism/P04_TestMethodOverriding.java
@@ -0,0 +1,33 @@
+package OOPs.M05_Polymorphism;
+
+/*
+In this example, we have defined the run method in the subclass as defined in the parent class but it has some specific implementation.
+
+The name and parameter of the method are the same, and there is IS-A relationship between the classes, so there is method overriding.
+*/
+
+
+class Vehicle {
+    //defining a method
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+// creating a child class
+class Bike2 extends Vehicle {
+    //defining the same method as in the parent class
+    void run() {
+        System.out.println("Bike is running safely...");
+    }
+}
+
+
+
+
+public class P04_TestMethodOverriding {
+    public static void main(String[] args) {
+        Bike2 b = new Bike2();
+        b.run();
+    }
+}
