87b0e2a90bc1706488b7d9543b3bda70aad73760
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-07 16:41:51 +0530

    super keyword

---
+++ M06_Super_Keyword/P03_TestSuper.java
@@ -0,0 +1,25 @@
+package OOPs.M06_Super_Keyword;
+
+class Player {
+
+    //creating constructor
+    Player() {
+        System.out.println("Player is playing");
+    }
+
+}
+
+
+// creating child class 
+class Virat extends Player {
+    Virat() {
+        super(); // super keyword to invoke the parent class constructor
+        System.err.println("Virat is playing");
+    }
+}
+
+public class P03_TestSuper {
+    public static void main(String[] args) {
+        Virat v = new Virat();
+    }
+}
