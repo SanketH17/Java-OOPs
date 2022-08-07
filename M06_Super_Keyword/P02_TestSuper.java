c3da3989e423ef33f33ceef119782ff1421ed10a
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-07 16:34:24 +0530

    super keyword

---
+++ M06_Super_Keyword/P02_TestSuper.java
@@ -0,0 +1,44 @@
+package OOPs.M06_Super_Keyword;
+
+
+class Animal10 {
+    void eat() {
+        System.out.println("eating...");
+    }
+    void live() {
+        System.out.println("Live in house...");
+    }
+}
+
+
+class Dog extends Animal10 {
+    void eat() {
+        System.out.println("eating bread....");
+    }
+
+    void bark() {
+        System.out.println("barking...");
+    }
+
+    void work() {
+        super.eat();
+        super.live();
+        bark();
+    }
+}
+
+
+
+public class P02_TestSuper {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.work();
+    }
+}
+
+
+
+
+
+
+
